20cda660d8644df9cd6df55a41d368b202de4d8d
Simon Que <simonque@gmail.com>
2014-02-16 18:15:47 -0800

    firmware: Add utils header file

---
+++ firmware/utils.h
@@ -0,0 +1,25 @@
+// Copyright (C) 2014 Simon Que
+//
+// This file is part of DuinoCube.
+//
+// DuinoCube is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// DuinoCube is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with DuinoCube.  If not, see <http://www.gnu.org/licenses/>.
+
+// Misc utils.
+
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#define ARRAY_SIZE(array) (sizeof(array) / sizeof(array[0]))
+
+#endif  // __UTILS_H__
